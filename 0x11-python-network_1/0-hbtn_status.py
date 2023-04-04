f317e60be8cb8bb7f26384edcfe86b1f0ac75713
Kembu <allantoo05@gmail.com>
2023-04-04 14:16:15 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == '__main__':
+    """Opening a url with a context manager"""
+    import urllib.request
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as f:
+        html = f.read()
+        print('Body response:')
+        print(f'\t- type: {type(html)}')
+        print(f'\t- content: {html}')
+        print(f'\t- utf8 content: {html.decode("utf-8")}')
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+This is where 0x11. Python - Network #1 tasks will be done
