9ddd639a8c79f560df49e83b21c7618b02d10b2e
Kembu <allantoo05@gmail.com>
2023-04-04 14:20:17 -0700

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == '__main__':
+    import sys
+    from urllib import request, parse
+    kv = {'email': sys.argv[2]}
+    data = parse.urlencode(kv).encode()
+    req = request.Request(sys.argv[1], data=data)
+    with request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
