5e3aaeadecd5ae443def30d8b26880ca28aadc4f
Kembu <allantoo05@gmail.com>
2023-04-04 14:18:36 -0700

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == '__main__':
+    import sys
+    from urllib.request import urlopen
+    with urlopen(sys.argv[1]) as f:
+        print(f.getheader('X-Request-Id'))
