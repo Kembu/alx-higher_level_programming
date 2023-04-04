91b8068162667bda82bc0a13373305067069e7f9
Kembu <allantoo05@gmail.com>
2023-04-04 14:37:10 -0700

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Getting a request using requests package"""
+
+import requests
+from sys import argv as av
+from requests.auth import HTTPBasicAuth
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    try:
+        user = av[1]
+        pswd = av[2]
+    except IndexError:
+        print('Please enter a username and password')
+        exit()
+    auth = HTTPBasicAuth(user, pswd)
+
+    auth = (user, pswd)
+
+    res = requests.get(url, auth=auth)
+    print(res.json().get('id'))
